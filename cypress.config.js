6ed8dabce1b7aa8a4b600aa6536a810cbe89713a
conversaShawn <shawn.harris3510@gmail.com>
2022-04-17 04:36:46 -0700

    adjust pid location

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,13 @@ const { defineConfig } = require("cypress");
 const { devServer } = require("@cypress/react/plugins/react-scripts");
 
 module.exports = defineConfig({
+  projectId: "enr2tk",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "enr2tk",
   },
   component: {
     devServer,
-    projectId: "enr2tk",
   },
 });
