972574793a742c18e88d6997f81bebea472825d4
conversaShawn <shawn.harris3510@gmail.com>
2022-04-17 06:08:38 -0700

    testing e2e config

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 const { devServer } = require("@cypress/react/plugins/react-scripts");
 
 module.exports = defineConfig({
-  projectId: "enr2tk",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
